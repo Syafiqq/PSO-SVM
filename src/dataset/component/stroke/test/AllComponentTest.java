ff9b72b21f84eb7b93bda0e91e59b036b48edcfb
Muhammad Syafiq <Syafiq.rezpector@gmail.com>
2016-08-13 09:52:06 +0700

    Create new StrokeData unit testing

---
+++ src/dataset/component/stroke/test/AllComponentTest.java
@@ -0,0 +1,16 @@
+package dataset.component.stroke.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by Muhammad Syafiq on 8/13/2016.
+ * Email : Syafiq.rezpector@gmail.com
+ * GitHub : Syafiqq
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({StrokeDataTest.class, StrokeMetadataTest.class, StrokeParameterTest.class})
+public class AllComponentTest
+{
+
+}
