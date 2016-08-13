43d328e3b77b6377685f476b85a89462a66c8e9c
Muhammad Syafiq <Syafiq.rezpector@gmail.com>
2016-08-13 09:53:26 +0700

    Create cumulate test for Dataset package

---
+++ src/dataset/test/CumulativeTest.java
@@ -0,0 +1,15 @@
+package dataset.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Created by Muhammad Syafiq on 8/13/2016.
+ * Email : Syafiq.rezpector@gmail.com
+ * GitHub : Syafiqq
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({AllComponentTest.class, dataset.component.stroke.test.AllComponentTest.class, dataset.component.test.AllComponentTest.class})
+public class CumulativeTest
+{
+}
