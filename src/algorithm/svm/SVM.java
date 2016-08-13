5b606b6bb6e46018bb0054d8b010fb0fb5449cab
Muhammad Syafiq <Syafiq.rezpector@gmail.com>
2016-08-13 08:56:58 +0700

    Add SVM class and import org.apache.commons.math:3.6 library

---
+++ src/algorithm/svm/SVM.java
@@ -0,0 +1,52 @@
+package algorithm.svm;
+
+import dataset.component.StrokeData;
+
+/**
+ * Created by Muhammad Syafiq on 8/13/2016.
+ * Email : Syafiq.rezpector@gmail.com
+ * GitHub : Syafiqq
+ */
+public class SVM
+{
+    private final double augmentingFactor;
+    private final double learningRate;
+    private final double constantCost;
+    private final StrokeData[] training;
+    private final StrokeData[] testing;
+    private double[] variance;
+
+    public SVM(double augmentingFactor, double learningRate, double constantCost, StrokeData[] training, StrokeData[] testing)
+    {
+        this.augmentingFactor = augmentingFactor;
+        this.learningRate = learningRate;
+        this.constantCost = constantCost;
+        this.training = training;
+        this.testing = testing;
+    }
+
+    public double getAugmentingFactor()
+    {
+        return augmentingFactor;
+    }
+
+    public double getLearningRate()
+    {
+        return learningRate;
+    }
+
+    public double getConstantCost()
+    {
+        return constantCost;
+    }
+
+    public StrokeData[] getTraining()
+    {
+        return training;
+    }
+
+    public StrokeData[] getTesting()
+    {
+        return testing;
+    }
+}
