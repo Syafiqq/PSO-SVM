ac0530e30a4d1a5e6509f6bc330cda310a501633
Muhammad Syafiq <Syafiq.rezpector@gmail.com>
2016-08-15 15:56:41 +0700

    Add dataset generator test for parameter

---
+++ src/dataset/test/DatasetGeneratorTestForParameter.java
@@ -0,0 +1,61 @@
+package dataset.test;
+
+import dataset.DatasetGenerator;
+import dataset.component.core.Dataset;
+import dataset.component.core.DatasetConverter;
+import main.Main;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Muhammad Syafiq on 8/15/2016.
+ * Email : Syafiq.rezpector@gmail.com
+ * GitHub : Syafiqq
+ */
+@SuppressWarnings("Duplicates") public class DatasetGeneratorTestForParameter
+{
+    @Parameterized.Parameter
+    private Dataset dataset;
+    @Parameterized.Parameter
+    private DatasetConverter<LinkedHashMap<Integer, Integer>> encoder;
+    @Parameterized.Parameter
+    private DatasetConverter<LinkedHashMap<Integer, Integer>> decoder;
+    @Parameterized.Parameter
+    private DatasetGenerator generator;
+
+    @Before
+    public void initialize()
+    {
+        Main.getMySqliteProperties();
+        this.dataset = new Dataset();
+        this.encoder = new DatasetConverter<>();
+        this.decoder = new DatasetConverter<>();
+        this.generator = new DatasetGenerator(this.dataset, this.encoder, this.decoder);
+        this.generator.generateDataset();
+    }
+
+    @Test
+    public void testStatus01()
+    {
+        System.out.println(Arrays.toString(this.dataset.getParameter()));
+    }
+
+    @Test
+    public void testStatusEncoder01()
+    {
+        System.out.println(this.encoder.getParameter().toString());
+    }
+
+    @Test
+    public void testStatusDecoder01()
+    {
+        System.out.println(this.decoder.getParameter().toString());
+    }
+
+}
