ec3d90c12b20a302c26e252029f265b42bde1924
Muhammad Syafiq <Syafiq.rezpector@gmail.com>
2016-08-15 15:24:00 +0700

    Add debug database configuration

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,7 +11,20 @@ public class Main
 {
     public static void getMySqliteProperties()
     {
+        Main.debugSetting();
+    }
+
+    private static void releaseSetting()
+    {
         final DBProperties properties = DBProperties.getInstance();
         properties.url = "jdbc:sqlite:D:/Muhammad Syafiq/Documents/JetBrains/Idea/SVM_001/db/stroke.mcrypt";
     }
+
+    private static void debugSetting()
+    {
+        final DBProperties properties = DBProperties.getInstance();
+        properties.url = "jdbc:sqlite:D:/Muhammad Syafiq/Documents/JetBrains/Idea/SVM_001/db/stroke_test01.mcrypt";
+    }
+
+
 }
