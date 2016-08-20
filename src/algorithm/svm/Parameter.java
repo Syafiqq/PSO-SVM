08ed6cb160cfe5dde248bc6df80d0f4cce8d5de3
Muhammad Syafiq <Syafiq.rezpector@gmail.com>
2016-08-20 07:54:27 +0700

    Create SVM Parameter component

---
+++ src/algorithm/svm/Parameter.java
@@ -0,0 +1,72 @@
+package algorithm.svm;
+
+/**
+ * Created by Muhammad Syafiq on 8/18/2016.
+ * Email : Syafiq.rezpector@gmail.com
+ * GitHub : Syafiqq
+ */
+public class Parameter
+{
+    private final int parameterSize;
+    private final int classTotal;
+    private final int multiplierIteration;
+    private double augmentingFactor;
+    private double learningRate;
+    private double constantCost;
+
+    public Parameter(double augmentingFactor, double learningRate, double constantCost, int parameterSize, int classTotal, int multiplierIteration)
+    {
+        this.updateParameter(augmentingFactor, learningRate, constantCost);
+        this.parameterSize = parameterSize;
+        this.classTotal = classTotal;
+        this.multiplierIteration = multiplierIteration;
+    }
+
+    public double getAugmentingFactor()
+    {
+        return augmentingFactor;
+    }
+
+    public double getLearningRate()
+    {
+        return learningRate;
+    }
+
+    public double getConstantCost()
+    {
+        return constantCost;
+    }
+
+    public int getParameterSize()
+    {
+        return parameterSize;
+    }
+
+    public int getClassTotal()
+    {
+        return classTotal;
+    }
+
+    public int getMultiplierIteration()
+    {
+        return this.multiplierIteration;
+    }
+
+    public void updateParameter(double augmentingFactor, double learningRate, double constantCost)
+    {
+        this.augmentingFactor = augmentingFactor;
+        this.learningRate = learningRate;
+        this.constantCost = constantCost;
+    }
+
+    @Override public String toString()
+    {
+        return "Parameter{" +
+                "augmentingFactor=" + augmentingFactor +
+                ", learningRate=" + learningRate +
+                ", constantCost=" + constantCost +
+                ", parameterSize=" + parameterSize +
+                ", classTotal=" + classTotal +
+                '}';
+    }
+}
