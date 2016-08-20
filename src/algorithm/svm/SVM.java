ffc78fa26e81476f1acc1c6f98d9eefba54647a0
Muhammad Syafiq <Syafiq.rezpector@gmail.com>
2016-08-20 07:55:35 +0700

    Completing SVM Structure and Calculation

--- src/algorithm/svm/SVM.java
+++ src/algorithm/svm/SVM.java
@@ -3,6 +3,10 @@ package algorithm.svm;
 import dataset.component.stroke.StrokeData;
 import dataset.component.stroke.StrokeParameter;
 import org.apache.commons.math3.stat.StatUtils;
+import org.apache.commons.math3.util.FastMath;
+
+import java.util.ArrayList;
+import java.util.ListIterator;
 
 /**
  * Created by Muhammad Syafiq on 8/13/2016.
@@ -11,36 +15,221 @@ import org.apache.commons.math3.stat.StatUtils;
  */
 public class SVM
 {
-    private final double augmentingFactor;
-    private final double learningRate;
-    private final double constantCost;
+    private final Parameter parameter;
     private final StrokeData[] training;
-    private final StrokeData[] testing;
-    private final StrokeParameter variance;
+    private final OneAgainstAll[] oneAgainstAlls;
+    private final double[] variance;
+    private final double[][] kernel;
+    private final double[] kernelTesting;
 
-    public SVM(double augmentingFactor, double learningRate, double constantCost, StrokeData[] training, StrokeData[] testing)
+    public SVM(final Parameter parameter, StrokeData[] training)
     {
-        this.augmentingFactor = augmentingFactor;
-        this.learningRate = learningRate;
-        this.constantCost = constantCost;
+        this.parameter = parameter;
         this.training = training;
-        this.testing = testing;
-        this.variance = new StrokeParameter(0, 0, 0, 0, 0);
+        this.variance = new double[this.parameter.getParameterSize()];
+        this.kernel = new double[this.training.length][this.training.length];
+        this.oneAgainstAlls = new OneAgainstAll[this.parameter.getClassTotal() - 1];
+        this.kernelTesting = new double[this.training.length];
+        this.initializeSVM();
     }
 
-    public double getAugmentingFactor()
+    private void initializeSVM()
     {
-        return augmentingFactor;
+        this.calculateVariance();
+        this.calculateKernelBetweenData();
+        this.generateOneAgainstAll();
     }
 
-    public double getLearningRate()
+    public void doSequentialLearning()
     {
-        return learningRate;
+        this.calculateMatrixD();
+        this.learnMultiplier();
+        this.calculateBias();
     }
 
-    public double getConstantCost()
+    public int doTesting(final StrokeData[] data)
     {
-        return constantCost;
+        int total = 0;
+        for(final StrokeData stroke : data)
+        {
+            total += doClassify(stroke.getParameterComponent()) == stroke.getMetadata().getStatus() ? 1 : 0;
+        }
+        return total;
+    }
+
+    public int doClassify(final StrokeParameter data)
+    {
+        this.calculateKernelDataTesting(data);
+        int clazz = 0;
+        for(final OneAgainstAll oaa : this.oneAgainstAlls)
+        {
+            if(oaa.doClassify(this.kernelTesting) == 1)
+            {
+                break;
+            }
+            else
+            {
+                ++clazz;
+            }
+        }
+        return clazz;
+    }
+
+    private void calculateKernelDataTesting(StrokeParameter data)
+    {
+        final double[] testing = data.getParameter();
+        final double[] kernel = this.kernelTesting;
+        final double[] variance = this.variance;
+        for(int trainingIndex = -1, trainingSize = this.training.length, parameterSize = this.parameter.getParameterSize(); ++trainingIndex < trainingSize; )
+        {
+            final double[] training = this.training[trainingIndex].getParameterComponent().getParameter();
+            double value = 1;
+            for(int parameterIndex = -1; ++parameterIndex < parameterSize; )
+            {
+                value *= FastMath.exp(FastMath.pow(testing[parameterIndex] - training[parameterIndex], 2) / (-2 * variance[parameterIndex]));
+            }
+            kernel[trainingIndex] = value;
+        }
+    }
+
+    private void calculateVariance()
+    {
+        final double[] variance = this.variance;
+        final double[] currentCalculation = new double[this.training.length];
+        for(int parameterIndex = -1, parameterSize = this.parameter.getParameterSize(), trainingSize = this.training.length; ++parameterIndex < parameterSize; )
+        {
+            for(int trainingIndex = -1; ++trainingIndex < trainingSize; )
+            {
+                currentCalculation[trainingIndex] = this.training[trainingIndex].getParameterComponent().getParameter(parameterIndex);
+            }
+            variance[parameterIndex] = StatUtils.variance(currentCalculation);
+        }
+    }
+
+    private void calculateKernelBetweenData()
+    {
+        final double[] variance = this.variance;
+        final double[][] kernel = this.kernel;
+        for(int trainingIndexLv1 = -1, trainingSize = this.training.length, parameterSize = this.parameter.getParameterSize(); ++trainingIndexLv1 < trainingSize; )
+        {
+            final double[] trainingX = this.training[trainingIndexLv1].getParameterComponent().getParameter();
+            for(int preTrainingIndexLv2 = -1; ++preTrainingIndexLv2 < trainingIndexLv1; )
+            {
+                kernel[trainingIndexLv1][preTrainingIndexLv2] = kernel[preTrainingIndexLv2][trainingIndexLv1];
+            }
+
+            kernel[trainingIndexLv1][trainingIndexLv1] = 1.0;
+
+            for(int postTrainingIndexLv2 = trainingIndexLv1; ++postTrainingIndexLv2 < trainingSize; )
+            {
+                final double[] trainingY = this.training[postTrainingIndexLv2].getParameterComponent().getParameter();
+
+                double value = 1;
+                for(int parameterIndex = -1; ++parameterIndex < parameterSize; )
+                {
+                    value *= FastMath.exp(FastMath.pow(trainingX[parameterIndex] - trainingY[parameterIndex], 2) / (-2 * variance[parameterIndex]));
+                }
+                kernel[trainingIndexLv1][postTrainingIndexLv2] = value;
+            }
+        }
+    }
+
+    private void generateOneAgainstAll()
+    {
+        final StrokeData[] training = this.training;
+        ArrayList<Integer> trainingNumber = new ArrayList<>(this.training.length);
+
+        /*
+        * Generate Training Index
+        * */
+        for(int trainingIndex = -1, trainingSize = training.length; ++trainingIndex < trainingSize; )
+        {
+            trainingNumber.add(trainingIndex);
+        }
+
+        /*
+        * Begin generate OneAgainstAll
+        * */
+        for(int oaaLevel = -1, oaaSize = this.oneAgainstAlls.length; ++oaaLevel < oaaSize; )
+        {
+            this.oneAgainstAlls[oaaLevel] = new OneAgainstAll(this.training.length);
+            this.oneAgainstAlls[oaaLevel].setAllowedData(trainingNumber.stream().mapToInt(Integer::valueOf).toArray());
+
+            final int[] clazz = this.oneAgainstAlls[oaaLevel].getClassData();
+
+            final ListIterator<Integer> dataNumberIt = trainingNumber.listIterator();
+            while(dataNumberIt.hasNext())
+            {
+                final int dataIndex = dataNumberIt.next();
+                if(training[dataIndex].getMetadata().getStatus() == oaaLevel)
+                {
+                    clazz[dataIndex] = 1;
+                    dataNumberIt.remove();
+                }
+                else
+                {
+                    clazz[dataIndex] = -1;
+                }
+            }
+        }
+    }
+
+    private void calculateMatrixD()
+    {
+        final double augmentingFactor = FastMath.pow(this.parameter.getAugmentingFactor(), 2);
+        /*
+        * Create temporary calculation
+        * */
+        final double[][] kernel = this.kernel;
+        final double[][] tempCalculation = new double[this.training.length][this.training.length];
+        for(int kernelIndexLv1 = -1, kernelSize = kernel.length; ++kernelIndexLv1 < kernelSize; )
+        {
+            for(int preKernelIndexLv2 = -1; ++preKernelIndexLv2 < kernelIndexLv1; )
+            {
+                tempCalculation[kernelIndexLv1][preKernelIndexLv2] = tempCalculation[preKernelIndexLv2][kernelIndexLv1];
+            }
+
+            for(int postKernelIndexLv2 = kernelIndexLv1 - 1; ++postKernelIndexLv2 < kernelSize; )
+            {
+                tempCalculation[kernelIndexLv1][postKernelIndexLv2] = (kernel[kernelIndexLv1][postKernelIndexLv2] + augmentingFactor);
+            }
+        }
+        /*
+        * Begin calculate matrixD
+        * */
+        for(final OneAgainstAll oaa : this.oneAgainstAlls)
+        {
+            oaa.calculateMatrixD(tempCalculation);
+        }
+    }
+
+    private void learnMultiplier()
+    {
+        for(final OneAgainstAll oaa : this.oneAgainstAlls)
+        {
+            oaa.learnMultiplier(this.parameter.getMultiplierIteration(), this.parameter.getLearningRate(), this.parameter.getConstantCost());
+        }
+    }
+
+
+    private void calculateBias()
+    {
+        for(final OneAgainstAll oaa : this.oneAgainstAlls)
+        {
+            int indexPlus = oaa.findGreatestMultiplierIndexByClassification(1);
+            int indexMinus = oaa.findGreatestMultiplierIndexByClassification(-1);
+            oaa.calculateBias(this.kernel[indexPlus], this.kernel[indexMinus]);
+        }
+    }
+
+    public double[][] getKernel()
+    {
+        return this.kernel;
+    }
+
+    public OneAgainstAll[] getOneAgainstAllLevel()
+    {
+        return this.oneAgainstAlls;
     }
 
     public StrokeData[] getTraining()
@@ -48,8 +237,8 @@ public class SVM
         return training;
     }
 
-    public StrokeData[] getTesting()
+    public double[] getVariance()
     {
-        return testing;
+        return variance;
     }
 }
